b65e9a11b35576fc01e7ff874fb0e6edff487bfc
Umar Asif <umarasif737@gmail.com>
2023-01-14 00:15:59 +0500

    useState used

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("Umar");
-  const [person, setPerson] = useState();
+  const [person, setPerson] = useState({ name: name, age: 22 });
+  const changeState = () => {
+    setName("Haseeb");
+    setPerson({ name: name, age: 24 });
+  };
   return (
     <View style={styles.container}>
-      <Text>M aay Firast App</Text>
+      <View>
+        <Text style={styles.text}>Name: {name}</Text>
+        <Text>
+          Name of Person is : {person.name} and his age is : {person.age}
+        </Text>
+      </View>
       <StatusBar style="auto" />
+
+      <View>
+        <Button title="Change the State" onPress={changeState} />
+      </View>
     </View>
   );
 }
