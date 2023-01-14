46dbeac37b25684ae7f36474450cf189c78393b1
Umar Asif <umarasif737@gmail.com>
2023-01-14 20:33:15 +0500

    TextInput learned

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("Umar");
-  const [person, setPerson] = useState({ name: name, age: 22 });
   const changeState = () => {
     setName("Haseeb");
     setPerson({ name: name, age: 24 });
@@ -13,15 +12,8 @@ export default function App() {
     <View style={styles.container}>
       <View>
         <Text style={styles.text}>Name {name}</Text>
-        <Text>
-          Name of Person is : {person.name} and his age is : {person.age}
-        </Text>
       </View>
-      <StatusBar style="auto" />
-
-      <View>
-        <Button title="Change the State" onPress={changeState} />
-      </View>
+      <TextInput style={styles.input} />
     </View>
   );
 }
@@ -32,5 +24,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: "black",
+    borderWidth: 1,
+    margin: 10,
+    padding: 101,
   },
 });
