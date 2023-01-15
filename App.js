b70e68e8d97635d7498865c610e7577f5752537e
Umar Asif <umarasif737@gmail.com>
2023-01-15 21:47:53 +0500

    Touchable opacity learned

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const handleChange = (e) => {
-    // console.log(e.target);
-    setName(e);
+  const [name, setName] = useState([
+    { name: "Umar", id: 1 },
+    { name: "Haseeb", id: 2 },
+    { name: "Usman", id: 3 },
+    { name: "Bilal", id: 4 },
+    { name: "Umara", id: 5 },
+    { name: "Haseeba", id: 6 },
+    { name: "Usmana", id: 7 },
+    { name: "Bilala", id: 8 },
+  ]);
+  const pressHandler = (id) => {
+    console.log(id);
+    setName(name.filter((item) => item.id !== id));
   };
+
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.text}>Name {name}</Text>
-      </View>
-      <TextInput
-        style={styles.input}
-        onChangeText={handleChange}
-        value={name}
-        name="todo"
-        placeholder="Add a todo"
+      <FlatList
+        numColumns={2}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+        data={name}
+        keyExtractor={(item) => item.id.toString()}
       />
     </View>
   );
@@ -28,15 +45,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
-  input: {
-    height: 40,
-    width: 200,
+  item: {
     borderColor: "black",
     borderWidth: 1,
-    margin: 10,
-    padding: 10,
+    margin: 20,
+    padding: 30,
+    backgroundColor: "pink",
+    color: "black",
+    fontSize: 20,
   },
 });
