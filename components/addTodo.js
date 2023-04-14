b20f5a0676a52032016a002c6b970a8f1438e04f
Umar Asif <umarasif737@gmail.com>
2023-04-15 00:48:25 +0500

    aaa

--- components/addTodo.js
+++ components/addTodo.js
@@ -33,5 +33,7 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
     borderBottomWidth: 1,
     borderBottomColor: "#ddd",
+    backgroundColor: "white",
+    borderRadius: 5,
   },
 });
